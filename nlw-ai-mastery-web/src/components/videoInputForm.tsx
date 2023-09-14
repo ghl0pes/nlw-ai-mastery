b7f210f5a23862791afcce57f19369967e497238
Gabriel Lopes <gabriellopes1005@gmail.com>
2023-09-14 15:15:34 -0300

    feat/front > status messages

--- nlw-ai-mastery-web/src/components/videoInputForm.tsx
+++ nlw-ai-mastery-web/src/components/videoInputForm.tsx
@@ -10,6 +10,13 @@ import { api } from "@/lib/axios";
 
 type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success';
 
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!'
+}
+
 export function VideoInputForm () {
   const [videoFile, setVideoFile] = useState<File | null>(null);
   const [status, setStatus] = useState<Status>('waiting');
@@ -128,9 +135,19 @@ export function VideoInputForm () {
         />
       </div>
 
-      <Button disabled={status !== 'waiting'} type="submit" className="w-full">
-        Carregar vídeo
-        <Upload className="w-4 h-4 ml-2"/>
+      <Button 
+        data-success={status === 'success'}
+        disabled={status !== 'waiting'} 
+        type="submit" 
+        className="w-full data-[success=true]:bg-emerald-400"
+      >
+        {status === 'waiting' ? (
+          <>
+            Carregar vídeo
+            <Upload className="w-4 h-4 ml-2"/>
+          </>
+        ) : statusMessages[status]
+        }
       </Button>
     </form>
   );
