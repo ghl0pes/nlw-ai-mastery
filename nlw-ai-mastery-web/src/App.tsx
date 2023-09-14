09520a1dc1e9ae4b83de59895a9bdaafcec29343
Gabriel Lopes <gabriellopes1005@gmail.com>
2023-09-14 15:37:30 -0300

    feat/front > temperature and videoId to App.tsx

--- nlw-ai-mastery-web/src/App.tsx
+++ nlw-ai-mastery-web/src/App.tsx
@@ -6,9 +6,18 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/videoInputForm";
+import { PromptSelect } from "./components/promptSelect";
+import { useState } from "react";
 
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5);
+  const [videoId, setVideoId] = useState<string | null>(null);
+
+  function handlePromptSelect (template: string) {
+    console.log(template)
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -48,22 +57,14 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
           <form className="space-y-6">
           <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value={"title"}>Título do YouTube</SelectItem>
-                  <SelectItem value={"description"}>Descrição do Youtube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelect}/>
             </div>
 
             <div className="space-y-2">
@@ -89,6 +90,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
               <span className="block text-xs text-muted-foreground italic">
                 Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.
