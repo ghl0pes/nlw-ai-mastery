2392f34faea831994b77fbac667a8dcaa618edab
Gabriel Lopes <gabriellopes1005@gmail.com>
2023-09-14 13:33:37 -0300

    Revert "feat/front > Header component"
    This reverts commit 23bac1fb508727760550f81fa1dd7d8297a76b67.

--- nlw-ai-mastery-web/src/App.tsx
+++ nlw-ai-mastery-web/src/App.tsx
@@ -5,13 +5,27 @@ import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
-import { Header } from "./children/header";
 
 
 export function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-small text-muted-foreground">
+            Desenvolvido com 💜 no NLW da RocketSeat
+          </span>
+
+          <Separator orientation="vertical" className="h-6"/>
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2"/>
+            Github
+          </Button>
+        </div>
+      </div>
 
       <main className="flex-1 p-6 flex gap-6">
         <div className="flex flex-col flex-1 gap-4">
